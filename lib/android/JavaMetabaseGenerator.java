023e844661674dc95f911c6d1182e8054ef2e8f7
Christopher Williams <cwilliams@appcelerator.com>
2014-04-24 13:36:28 -0400

    - exclude com.oracle.*, org.json.*, jdk.internal.*, org.jcp.*, org.ietf.* classes - Modify code to use a JSONWriter to stream the JSON out as we go rather than build up a huge JSONObject model. This should help avoid using up tons of RAM and out of memory errors. - Modify the JSON structure for class methods. A method is keyed by it's name, but it's value is an array of objects, each one representing a different signature of that method name. This should allow us to store overloaded methods with different signatures but the same name, rathe rthan clobbering them so last one we encounter wins. Fixes #334

--- lib/android/JavaMetabaseGenerator.java
+++ lib/android/JavaMetabaseGenerator.java
@@ -1,43 +1,69 @@
 /**
  * Java Metabase Generator
  */
-import java.io.*;
-import java.util.*;
-import java.util.jar.*;
-import java.util.zip.*;
-import java.util.regex.*;
-import org.apache.bcel.util.*;
-import org.apache.bcel.classfile.*;
-import org.apache.bcel.generic.*;
-import org.json.*;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.jar.JarFile;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.apache.bcel.classfile.AccessFlags;
+import org.apache.bcel.classfile.ExceptionTable;
+import org.apache.bcel.classfile.Field;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.Method;
+import org.apache.bcel.generic.Type;
+import org.apache.bcel.util.ClassPath;
+import org.apache.bcel.util.SyntheticRepository;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONWriter;
 
 /**
  * Class that will generate a metadatabase from the Java classpath
  */
 public class JavaMetabaseGenerator
 {
-    private static final SyntheticRepository repo = SyntheticRepository.getInstance();
-    private static final Pattern isClass = Pattern.compile("\\.class$");
+	private static final SyntheticRepository repo = SyntheticRepository.getInstance();
+	private static final Pattern isClass = Pattern.compile("\\.class$");
 
-    /**
-     * this is a regular expression of packages that we want to blacklist and not include in the output
-     */
-    private static final Pattern blacklist = Pattern.compile("^(javax\\/|com\\/sun|org\\/apache\\/bcel|sun\\/|com\\/apple|quicktime\\/|apple\\/|com\\/oracle\\/jrockit|oracle\\/jrockit|sunw\\/|org\\/omg|java\\/awt|java\\/applet|junit\\/|edu\\/umd\\/cs\\/findbugs)");
+	/**
+	 * this is a regular expression of packages that we want to blacklist and not include in the output
+	 */
+	private static final Pattern blacklist = Pattern
+			.compile("^(javax\\/|com\\/sun|com\\/oracle|jdk\\/internal|org\\/apache\\/bcel|org\\/jcp|org\\/json|org\\/ietf|sun\\/|com\\/apple|quicktime\\/|apple\\/|com\\/oracle\\/jrockit|oracle\\/jrockit|sunw\\/|org\\/omg|java\\/awt|java\\/applet|junit\\/|edu\\/umd\\/cs\\/findbugs)");
 
 	/**
 	 * enumerate over a zip/jar and load up it's classes
 	 */
-	private static void enumerate (Enumeration<? extends ZipEntry> e, HashMap<String,JavaClass> cache) throws Exception {
+	private static void enumerate(Enumeration<? extends ZipEntry> e, JSONWriter writer, Set<String> uniques)
+			throws Exception
+	{
 
-		for (;e.hasMoreElements();)
-       	{
-       		String entry = e.nextElement().toString();
-       		if (!blacklist.matcher(entry).find() && isClass.matcher(entry).find()) {
-       			String classname = entry.replaceAll("/",".").replace(".class","");
-       			JavaClass cls = repo.loadClass(classname);
-       			cache.put(classname, cls);
-       		}
-       	}
+		while (e.hasMoreElements())
+		{
+			String entry = e.nextElement().toString();
+			if (!blacklist.matcher(entry).find() && isClass.matcher(entry).find())
+			{
+				String classname = entry.replaceAll("/", ".").replace(".class", "");
+				if (uniques.contains(classname))
+				{
+					continue;
+				}
+
+				JavaClass cls = repo.loadClass(classname);
+				writer.key(classname);
+				writer.object();
+				asJSON(cls, writer);
+				writer.endObject();
+
+				uniques.add(classname);
+			}
+		}
 	}
 
 	/**
@@ -81,94 +107,118 @@ public class JavaMetabaseGenerator
 	/**
 	 * this class takes no arguments and returns JSON as System.out
 	 */
-    public static void main(String[] args) throws Exception
-    {
+	public static void main(String[] args) throws Exception
+	{
+		ClassPath cp = new ClassPath();
+		String classpath = cp.getClassPath();
+		String tokens[] = classpath.split(File.pathSeparator);
 
-    	ClassPath cp = new ClassPath();
-    	String classpath = cp.getClassPath();
-    	String tokens [] = classpath.split(File.pathSeparator);
-    	HashMap<String,JavaClass> cache = new HashMap<String,JavaClass>();
-    	for (String token : tokens)
-    	{
-    		if (token.endsWith(".jar")) {
-    			JarFile jarFile = new JarFile(token);
-    			enumerate(jarFile.entries(),cache);
-    		}
-    		else if (token.endsWith(".zip")) {
-    			ZipFile zipFile = new ZipFile(token);
-    			enumerate(zipFile.entries(),cache);
-    		}
-    	}
+		PrintWriter pw = new PrintWriter(System.out, true);
+		JSONWriter writer = new JSONWriter(pw);
+		writer.object();
+		writer.key("classes");
+		writer.object();
+		Set<String> uniques = new HashSet<String>();
+		for (String token : tokens)
+		{
+			if (token.endsWith(".jar"))
+			{
+				JarFile jarFile = new JarFile(token);
+				enumerate(jarFile.entries(), writer, uniques);
+			}
+			else if (token.endsWith(".zip"))
+			{
+				ZipFile zipFile = new ZipFile(token);
+				enumerate(zipFile.entries(), writer, uniques);
+			}
+		}
+		writer.endObject();
+		writer.endObject();
+		pw.flush();
+	}
 
-    	JSONObject json = new JSONObject();
-    	JSONObject classes = new JSONObject();
-    	json.put("classes",classes);
+	private static void asJSON(JavaClass javaClass, JSONWriter writer)
+	{
+		// package
+		writer.key("package");
+		writer.value(javaClass.getPackageName());
 
-    	for (String key : cache.keySet())
-    	{
-    		JavaClass javaClass = cache.get(key);
-    		JSONObject classJSON = new JSONObject();
-    		classJSON.put("package",javaClass.getPackageName());
-    		String interfaces [] = javaClass.getInterfaceNames();
-    		JSONArray interfacesJSON = new JSONArray();
-    		for (String intfn : interfaces)
-    		{
-    			interfacesJSON.put(intfn);
-    		}
-    		classJSON.put("interfaces",interfacesJSON);
-    		classJSON.put("superClass",javaClass.getSuperclassName());
-    		classJSON.put("attributes",addAttributes(javaClass));
-    		classJSON.put("metatype",javaClass.isInterface() ? "interface" : "class");
-    		classes.put(key, classJSON);
+		// interfaces
+		writer.key("interfaces");
+		writer.array();
+		for (String intfn : javaClass.getInterfaceNames())
+		{
+			writer.value(intfn);
+		}
+		writer.endArray();
 
-    		JSONObject methodsJSON = new JSONObject();
-    		classJSON.put("methods", methodsJSON);
+		// superclass
+		writer.key("superClass");
+		writer.value(javaClass.getSuperclassName());
 
-    		Method methods [] = javaClass.getMethods();
-    		for (Method method : methods)
-    		{
-    			JSONObject methodJSON = new JSONObject();
-    			methodJSON.put("attributes",addAttributes(method));
-    			methodsJSON.put(method.getName(), methodJSON);
-    			JSONArray argumentJSON = new JSONArray();
-    			for (Type type : method.getArgumentTypes())
-    			{
-    				JSONObject obj = new JSONObject();
-    				obj.put("type",type);
-    				argumentJSON.put(obj);
-    			}
-    			methodJSON.put("args",argumentJSON);
-    			methodJSON.put("returnType",method.getReturnType());
-    			JSONArray exceptionsJSON = new JSONArray();
-    			ExceptionTable exceptions = method.getExceptionTable();
-    			if (exceptions!=null)
-    			{
-    				for (String exname : exceptions.getExceptionNames())
-    				{
-    					exceptionsJSON.put(exname);
-    				}
-    			}
-    			methodJSON.put("exceptions",exceptionsJSON);
-    		}
+		// attributes
+		writer.key("attributes");
+		writer.value(addAttributes(javaClass));
 
+		// metatype
+		writer.key("metatype");
+		writer.value(javaClass.isInterface() ? "interface" : "class");
 
-    		JSONObject propertiesJSON = new JSONObject();
-    		classJSON.put("properties", propertiesJSON);
+		// methods
+		writer.key("methods");
+		JSONObject methodsJSON = new JSONObject();
+		Method methods[] = javaClass.getMethods();
+		for (Method method : methods)
+		{
+			JSONObject methodJSON = new JSONObject();
+			methodJSON.put("attributes", addAttributes(method));
+			JSONArray overloads;
+			if (methodsJSON.has(method.getName()))
+			{
+				overloads = methodsJSON.getJSONArray(method.getName());
+			}
+			else
+			{
+				overloads = new JSONArray();
+			}
+			overloads.put(methodJSON);
+			methodsJSON.put(method.getName(), overloads);
+			JSONArray argumentJSON = new JSONArray();
+			for (Type type : method.getArgumentTypes())
+			{
+				JSONObject obj = new JSONObject();
+				obj.put("type", type);
+				argumentJSON.put(obj);
+			}
+			methodJSON.put("args", argumentJSON);
+			methodJSON.put("returnType", method.getReturnType());
+			JSONArray exceptionsJSON = new JSONArray();
+			ExceptionTable exceptions = method.getExceptionTable();
+			if (exceptions != null)
+			{
+				for (String exname : exceptions.getExceptionNames())
+				{
+					exceptionsJSON.put(exname);
+				}
+			}
+			methodJSON.put("exceptions", exceptionsJSON);
+		}
+		writer.value(methodsJSON);
 
-    		Field fields [] = javaClass.getFields();
-    		for (Field field : fields)
-    		{
-    			JSONObject fieldJSON = new JSONObject();
-    			fieldJSON.put("attributes",addAttributes(field));
-    			fieldJSON.put("type",field.getType());
-    			fieldJSON.put("value",field.getConstantValue());
-    			fieldJSON.put("metatype", field.getConstantValue()!=null ? "constant" : "field");
-    			fieldJSON.put("attributes",addAttributes(field));
-    			propertiesJSON.put(field.getName(), fieldJSON);
-    		}
-
-    	}
-
-    	System.out.println(json.toString(3));
-    }
+		// properties
+		writer.key("properties");
+		JSONObject propertiesJSON = new JSONObject();
+		Field fields[] = javaClass.getFields();
+		for (Field field : fields)
+		{
+			JSONObject fieldJSON = new JSONObject();
+			fieldJSON.put("attributes", addAttributes(field));
+			fieldJSON.put("type", field.getType());
+			fieldJSON.put("value", field.getConstantValue());
+			fieldJSON.put("metatype", field.getConstantValue() != null ? "constant" : "field");
+			fieldJSON.put("attributes", addAttributes(field));
+			propertiesJSON.put(field.getName(), fieldJSON);
+		}
+		writer.value(propertiesJSON);
+	}
 }
